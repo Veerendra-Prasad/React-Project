3689bcd6d27ea4741e4b5b2e1bb04ae08357d673
Veerendra Prasad <veerendraprasadkarnam27@gmail.com>
2024-09-03 11:55:23 +0530

    minor changes

--- src/components/4-Grocery/Message.jsx
+++ src/components/4-Grocery/Message.jsx
@@ -14,9 +14,6 @@ function Message({msg , remove}) {
     <div className="w-full p-3  flex justify-center items-center ">
       <div className="w-full sm:w-1/3 bg-slate-200 flex justify-center items-center p-3 rounded-md">
         <h1>{msg}</h1>
-        <button className="ml-auto bg-sky-400 w-7 border-2 border-black rounded-md text-white">
-          X
-        </button>
       </div>
     </div>
   );
